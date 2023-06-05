1f43616f3a1fdf2df707b01d639652a49a581b08
NekitaLucy <nekitalucy2015@gmail.com>
2023-06-04 21:42:35 -0400

    commit
    update and added JAvascript

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+//This is a single line comment, started with two slashes
+
+/* This is a multi-line comment
+Everything in here has no effect.
+var test = 'This will not actually create a variable';
+*/
+//Everything after the slashes has no effect
+
+console.log("Bonjour, Hello. Welcome to my UI Website Portfolio!")
+alert("Bonjour, Hello. Welcome to my UI Website Portfolio!");
