b3143ed3d283ada6dc8cba9cd3f594c5a8ef81f4
NekitaLucy <nekitalucy2015@gmail.com>
2023-06-11 11:05:28 -0400

    JAVASCRIPT
    If applied, this commit will  make the page mobile responsive for a hamburger icon

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,7 @@
-//This is a single line comment, started with two slashes
 
-/* This is a multi-line comment
-Everything in here has no effect.
-var test = 'This will not actually create a variable';
-*/
-//Everything after the slashes has no effect
 
 console.log("Bonjour, Hello. Welcome to my UI Website Portfolio!")
 alert("Bonjour, Hello. Welcome to my UI Website Portfolio!");
-
-
-console.log(text1, text2, text3)
-
-var userInput = "";
-if (userInput) {
-  // Do something with the user input!
-}
-
-<script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
 
 
 function menuToggle() {
